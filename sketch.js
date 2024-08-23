9e24e7e136ecb2b9ca6d076493316d407b0ab2f0
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-23 01:10:26 -0300

    troca de varias variaveis para objetos(chão, background, dino, rock), add mostrador de score e retirada das informações de contagem de fps e tamanho da tela

--- sketch.js
+++ sketch.js
@@ -1,110 +1,165 @@
-let walk = [], run = [], jump = [], dead = [];
-let cont = 0, deslocamento = 0;
-let dinoPositionX = 50, dinoPositionY = 408;
-let dinoWidth = 180, dinoHeight = 180;
-let move = 'walk';
-let floor,bg,rock,tree,jumpStartTime,assinatura;
+let cont = 0, score = 0, velocidade = 10, deslocamentoRock01 = 0, deslocamentoRock02 = 0
+let walk = [], run = [], jump = [], dead = []
 
-const jumpFrames = 12, jumpDuration = 0.9, jumpHeight = 200; 
+const dino = {
+  xPos: 50,
+  yPos: 410,
+  width: 180,
+  height: 180,
+  move: 'walk'
+}
+
+const rock = {
+  img: null,
+  xPos: window.innerWidth - 150,
+  yPos: 500,
+  width: 80,
+  height: 80
+}
+
+const chao = {
+  img: null,
+  xPos: 50,
+  yPos: 570,
+  width: 80,
+  height: 100
+}
+
+const fundo = {
+  img: null,
+  xPos: 0,
+  yPos: 0,
+  width: 1400,
+  height: window.innerHeight + 120
+}
+
+const assinatura = {
+  img: null,
+  xPos: window.innerWidth - 300,
+  yPos: 15,
+  width: 250,
+  height: 20
+}
+
+const pulo = {
+  frames: 12,
+  duracao: 0.9,
+  altura: 100
+}
+
+let move = 'walk'
+let jumpStartTime = 0
 
 function preload() {
   for (let i = 1; i <= 10; i++) {
-    walk.push(loadImage(`images/dino/walk/Walk(${i}).png`));
+    walk.push(loadImage(`images/dino/walk/Walk(${i}).png`))
   }
   for (let i = 1; i <= 8; i++) {
-    run.push(loadImage(`images/dino/run/run(${i}).png`));
+    run.push(loadImage(`images/dino/run/run(${i}).png`))
   }
   for (let i = 1; i <= 12; i++) {
-    jump.push(loadImage(`images/dino/jump/jump(${i}).png`));
+    jump.push(loadImage(`images/dino/jump/jump(${i}).png`))
   }
   for (let i = 1; i <= 8; i++) {
-    dead.push(loadImage(`images/dino/dead/dead(${i}).png`));
+    dead.push(loadImage(`images/dino/dead/Dead(${i}).png`))
   }
 
-  floor = loadImage('images/cenario/floor.jpg');
-  bg = loadImage('images/cenario/bg-02.jpg');
-  rock = loadImage('images/cenario/obstaculos/rock.webp')
-  tree = loadImage('images/cenario/obstaculos/tree.png')
-  assinatura =  loadImage('images/assinatura.png')
-  
+  rock.img = loadImage('images/cenario/obstaculos/rock.png')
+  chao.img = loadImage('images/cenario/floor.jpg')
+  fundo.img = loadImage('images/cenario/bg-02.jpg')
+  assinatura.img = loadImage('images/assinatura.png')
 }
 
 function handleJump() {
-  let elapsedTime = (millis() - jumpStartTime) / 1000; 
-  let progress = elapsedTime / jumpDuration; 
-  let jumpProgress = Math.sin(progress * Math.PI); 
+  const elapsedTime = (millis() - jumpStartTime) / 1000
+  const progress = elapsedTime / pulo.duracao
+  const jumpProgress = Math.sin(progress * Math.PI)
+  
+  dino.yPos = 410 - (pulo.altura * jumpProgress)
 
-  dinoPositionY = 408 - (jumpHeight * jumpProgress);
-
-  let jumpFrame = Math.floor((elapsedTime / jumpDuration) * jumpFrames) % jumpFrames;
-  image(jump[jumpFrame], dinoPositionX, dinoPositionY, dinoWidth, dinoHeight);
+  const jumpFrame = Math.floor(progress * pulo.frames) % pulo.frames
+  image(jump[jumpFrame], dino.xPos, dino.yPos, dino.width, dino.height)
 
   if (progress >= 1) {
-    move = 'walk';
-    dinoPositionY = 408; 
+    move = 'walk'
+    dino.yPos = 410
   }
 }
 
 function drawFloor() {
   for (let i = 0; i < window.innerWidth; i += 80) {
-    image(floor, i, 570, 80, 100);
+    image(chao.img, i, chao.yPos, chao.width, chao.height)
   }
 }
 
-function drawTree(deslocamento){
-  image(tree,window.innerWidth - deslocamento,408,60,180)
-}
-function drawRock(deslocamento){
-  image(rock,window.innerWidth - deslocamento,516,80,80)
+function drawRock(xPos) {
+  image(rock.img, xPos, rock.yPos, rock.width, rock.height)
 }
 
 function startJump() {
-  move = 'jump';
-  jumpStartTime = millis();
+  move = 'jump'
+  jumpStartTime = millis()
 }
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  frameRate(15); 
+  createCanvas(window.innerWidth, window.innerHeight)
+  frameRate(15)
 
   document.addEventListener('keydown', function (event) {
-    if (event.key === 'ArrowUp' && move !== 'jump') {
-      startJump();
+    if ((event.key === 'ArrowUp' || event.key === ' ') && move !== 'jump') {
+      startJump()
     }
-  });
+  })
 
-  document.addEventListener('touchstart', function(event) {
+  document.addEventListener('touchstart', function () {
     if (move !== 'jump') {
-      startJump();
+      startJump()
     }
-  });
+  })
+}   
+
+function checkCollision(xPos , yPos) {
+  return (
+    dino.xPos == xPos + rock.width &&
+    dino.xPos + dino.width == xPos &&
+    dino.yPos + dino.height == yPos &&
+    dino.yPos == yPos + rock.height
+  )
 }
 
 function draw() {
-  deslocamento += 10
+  deslocamentoRock01 += velocidade
+  deslocamentoRock02 += velocidade
 
-  background('black');
-  image(bg,0,0,1400, window.innerHeight+120)
-  image(assinatura, window.innerWidth - 300, 15,250,20 )
+  background('black')
+
+  image(fundo.img, fundo.xPos, fundo.yPos, fundo.width, fundo.height)
+  image(assinatura.img, assinatura.xPos, assinatura.yPos, assinatura.width, assinatura.height)
+  
+  fill('white')
+  text(`Score: ${score}`, 50, 50)
 
   if (move === 'walk') {
-    cont = (cont + 1) % walk.length;
-    image(walk[cont], dinoPositionX, dinoPositionY, dinoWidth, dinoHeight);
+    cont = (cont + 1) % walk.length
+    image(walk[cont], dino.xPos, dino.yPos, dino.width, dino.height)
   } else if (move === 'jump') {
-    handleJump();
+    handleJump()
+  }
+
+  drawRock(window.innerWidth - deslocamentoRock01)
+  drawRock(window.innerWidth - deslocamentoRock02)
+
+  if (window.innerWidth - deslocamentoRock01 <= -rock.width) {
+    deslocamentoRock01 = Math.floor(Math.random() * 200) - window.innerWidth
+  }
+
+  if (window.innerWidth - deslocamentoRock02 <= -rock.width) {
+    deslocamentoRock02 = Math.floor(Math.random() * 200) - window.innerWidth
+    
   }
 
 
-  fill('white');
-  text(`${cont}`, 50, 50);
 
-  fill('red');
-  text(`${window.innerWidth}`, 150, 50);
-  fill('green');
-  text(`${window.innerHeight}`, 250, 50);
 
-  drawTree(deslocamento)
-  drawRock(deslocamento)
-
-  drawFloor();
+  drawFloor()
 }
